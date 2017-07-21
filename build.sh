c9dc1b355cacfd305a74e5c07f2e71576de2be17
alan-fu <ssisoo@live.cn>
2017-07-21 22:46:40 +0800

    原型模式

--- build.sh
+++ build.sh
@@ -11,3 +11,6 @@ javac -d ./out/ ./singleton/SingletonPatternDemo.java
 
 # build builder
 javac -d ./out/ ./builder/BuilderPatternDemo.java
+
+# build prototype
+javac -d ./out/ ./prototype/PrototypePatternDemo.java
