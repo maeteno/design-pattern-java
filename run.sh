a10c3269f9f95111905f0186ccf5bc15eb70cab9
alan-fu <ssisoo@live.cn>
2017-07-23 18:10:45 +0800

    新增运行脚本

---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# 进入目录
+cd ./out/
+
+# run factory
+java factory.FactoryPatternDemo
+
+# run abstractfactory
+java abstractfactory.AbstractFactoryPatternDemo
+
+# run singleton
+java singleton.SingletonPatternDemo
+
+# run builder
+java builder.BuilderPatternDemo
+
+# run prototype
+java prototype.PrototypePatternDemo
+
+# run adapter
+java adapter.AdapterPatternDemo
+
+cd ..
